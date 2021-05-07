cd48fc2e827f15a572b286b9113db80d25b530bf
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2021-05-06 22:35:31 -0300

    Micronaut Redis

--- src/mn-rate-limit/README.md
+++ src/mn-rate-limit/README.md
@@ -10,3 +10,9 @@
 
 - [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
 
+### Local Redis
+Redis v6 instance with exposed port 6379. Storage is ephemeral.
+
+`docker run --name my-redis -p 6379:6379 -d redis:6-alpine`
+
+https://hub.docker.com/_/redis
---
+++ src/mn-rate-limit/src/main/java/com/rafaelbaetapena/redis/RateLimitedTimeEndpoint.java
@@ -0,0 +1,60 @@
+package com.rafaelbaetapena.redis;
+
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Clock;
+import java.time.LocalTime;
+
+@Controller("/time")
+public class RateLimitedTimeEndpoint {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RateLimitedTimeEndpoint.class);
+    private static final int QUOTA_PER_MINUTE = 10;
+    private StatefulRedisConnection<String, String> redis;
+
+    public RateLimitedTimeEndpoint(StatefulRedisConnection<String, String> redis) {
+        this.redis = redis;
+    }
+
+    @ExecuteOn(TaskExecutors.IO)
+    @Get("/")
+    public String time() {
+        return getTime("EXAMPLE::TIME", LocalTime.now());
+    }
+
+    @ExecuteOn(TaskExecutors.IO)
+    @Get("/utc")
+    public String utc() {
+        return getTime("EXAMPLE::UTC", LocalTime.now(Clock.systemUTC()));
+    }
+
+    private String getTime(final String key,  final LocalTime now) {
+        final String value = redis.sync().get(key);
+        int currentQuota = null == value ? 0 : Integer.parseInt(value);
+        if (currentQuota >= QUOTA_PER_MINUTE) {
+            final String err = String.format("Rate limit reached %s %s/%s", key, currentQuota,
+                    QUOTA_PER_MINUTE);
+            LOG.info(err);
+            return err;
+        }
+        LOG.info("Current quota {} in {}/{}", key, currentQuota, QUOTA_PER_MINUTE);
+        increaseCurrentQuota(key);
+        return now.toString();
+    }
+
+    private void increaseCurrentQuota(String key) {
+        final RedisCommands<String, String> commands = redis.sync();
+        commands.multi();
+        commands.incrby(key, 1);
+        var remainingSeconds = 60 - LocalTime.now().getSecond();
+        commands.expire(key, remainingSeconds);
+        commands.exec();
+    }
+}
